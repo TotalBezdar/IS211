5ae2f022db8f29d38d8a45c2641b78ef396ae425
TotalBezdar <409-6@coopteh.local>
2024-10-26 15:52:18 +0700

    123

--- tests/RouterTest.php
+++ tests/RouterTest.php
@@ -8,7 +8,9 @@ class RouterTest extends TestCase {
     public function test_router() {
         $router = new Router();
         $html = $router->route( "http://localhost/orders" );
-        $pos= mb_strpos($html, "Содание заказа");
-       
+        $pos= mb_strpos($html, "Создаие заказа");
+        if((!($pos>=0)) and ($pos==false))
+            $pos= -1;
+        $this->assertNotFase( $pos>=0 );
     }
 }
